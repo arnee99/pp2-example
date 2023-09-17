485aeb0aeeb3bab383d2d77965b481d09b3da031
Arnur <116339748+arnee99@users.noreply.github.com>
2023-09-17 20:34:33 +0600

    Lab# 1 commit

---
+++ code/test.py
@@ -0,0 +1,118 @@
+# x = range(5)
+
+# if x < 10:
+#     print("x is less than 10")
+# elif x > 4 and x < 9:
+#     print("x is more than 4 and less than 9")
+# else:
+#     print("x is more than 10")
+    
+# for i in range(5):
+#     pass
+
+# while 3 < 4:
+#     pass
+
+# def my_function():
+#     x = 5
+#     y = 10
+    
+# class myClass:
+#     a = 5
+#     b = 6
+    
+#     def my_function2(self):
+#         '''
+#         This function is something funny!
+#         '''
+#         print(4 + 5)
+#         print(self.a + self.b)
+        
+# obj = myClass()
+# obj.my_function2().__doc__
+
+# ctrl + /, command + /
+
+# int a = 5;
+# char c = 'c';
+# string str = "Hello, world!";
+
+# a = 5
+# can only contain -> [a-Z0-9_]
+# starts with -> a-Z_
+# a != A
+
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# a = "some value"
+#a, b, c
+# a, b = 5, 10
+# int a = 10;
+# int b = 7;
+# int c = 0;
+# a -> b, b -> c, c -> a
+# a, b = b, a
+# print(a, b)
+
+# PI = 3.14159
+# MAX_VALUE = 100
+
+# a = 10 #int
+# print(type(a))
+# pi = 3.14 #float
+# print(type(pi))
+# z = 3 + 4j
+# print(type(z))
+
+# name1 = 'John Doe'
+# name2 = "Joanne Doe"
+# print((type(name1), type(name2)))
+
+# numbers = [1, 2, 3, 4, 5]
+# names = ['John Doe', "Joanne Doe"]
+# names[0] = 'Johny Depp'
+# print(numbers)
+# print(names)
+# random_list = [1, 2, 3, "John Doe", [1, 2, 3]]
+# print(random_list)
+
+# tup1 = (3, 4)
+# rgb = ('red', 'green', 'blue')
+# # tup1[0] = 'pink'
+# print(tup1)
+
+# student = {
+#     'name': 'John doe',
+#     'age': 24, 
+#     'id': '23B01012004'
+# }
+# print(student.keys())
+# print(student.values())
+
+# is_True = [1, 2, 3]
+# is_False = []
+# while is_False:
+#     print('Hello!')
+    
+# result = None
+# print(type(result))
+
+# a = 5              # the least priority
+# b = 3.14           # the middle priority
+# c = 2 + 3j         # the highest priority
+# d = b + c 
+# print(type(d))
+
+str1 = "hello"
+str2 = "World!"
+print(str1, end=" ") #concatenation
+print(str2)
